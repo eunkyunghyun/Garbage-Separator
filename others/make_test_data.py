a952aa068a5ebff95a8a62c59910d47a339590bc
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2021-10-04 00:10:48 +0900

    first commit

--- others/make_test_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-test_csv_path = "/tmp/pycharm/labels/test_vision.csv"
-tmp = pd.read_csv(test_csv_path, header=None)
-tmp["label"] = -1
-tmp.to_csv("/tmp/pycharm_project_917/labels/test_vision.csv", index=False, header=False)
